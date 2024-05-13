112b17980191683eac7fecaeb38a0b9a21781624
jvduartte <lia.c.santtos@gmail.com>
2024-05-13 17:30:44 -0300

    UP04

---
+++ 012-Laço_de_Repetição(doWhile).js
@@ -0,0 +1,24 @@
+// doWhile( laço faça enquanto)
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while ( i <= 5);
+
+const frutas = [ 'maçã', 'banana', 'laranja', 'pera','melancia'];
+let j = 0;
+do {
+    console.log( 'Eu gossto de ' + frutas[j]);
+    j++;
+} while (j < frutas.length);
+
+
+let k = 0;
+do {
+    if (k === 5){
+        break;
+    }
+    console.log(k);
+    k++;
+} while ( k < 10)
